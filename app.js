12027db822bcaa915c2f8446b7e74d4437090ac2
Ignjat Stjepanovic <53408313+Phalanxz@users.noreply.github.com>
2021-08-24 20:18:41 +0200

    Finished the memory game tutorial.

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const cards = document.querySelectorAll(".playcard");
 
+shuffle();
+
 let hasFlipped = false;
 let firstCard, secondCard;
+lockBoard = false;
 
 function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+    
   this.classList.toggle("flip");
 
   if (!hasFlipped) {
@@ -16,13 +22,29 @@ function flipCard() {
     if (firstCard.dataset.framework === secondCard.dataset.framework) {
       firstCard.removeEventListener("click", flipCard);
       secondCard.removeEventListener("click", flipCard);
+      resetBoard();
     } else {
+      lockBoard = true;
       setTimeout(() => {
         firstCard.classList.remove("flip");
         secondCard.classList.remove("flip");
+        lockBoard = false;
+        resetBoard();
       }, 750);
     }
   }
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false]
+  [firstCard, secondCard] = [null, null]
+}
+
+function shuffle() {
+  cards.forEach (card => {
+    let randomPos = Math.floor(Math.random() * 12)
+    card.style.order = randomPos;
+  });
 }
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
